11a3ce524c421b935603412989f4e791814c91ef
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-06-10 10:58:41 -0700

    revise js code and tweak errors

--- js/scripts.js
+++ js/scripts.js
@@ -4,31 +4,28 @@
 
 //BUISINESS LOGIC
 function checkForMatch(number) {
-  let numberArray = [];
-  for (let i = 0; number >= i; i++) {
-    function checkForMatch(number) {
-      let numberArray = [];
-      for (let i = 0; number >= i; i++) {
-        const element = i.toString();
-        if (element.includes('3')) {
-          numberArray.push("Won't you be my neighbor?");
-        } else if (element.includes('2')) {
-          numberArray.push('Boop!');
-        } else if (element.includes('1')) {
-          numberArray.push('Beep!');
-        } else {
-          numberArray.push(element);
-        }
-      }
-      return numberArray;
-    }
+	let numberArray = [];
+	for (let i = 0; number >= i; i++) {
+		const element = i.toString();
+		if (element.includes('3')) {
+			numberArray.push("Won't you be my neighbor?");
+		} else if (element.includes('2')) {
+			numberArray.push('Boop!');
+		} else if (element.includes('1')) {
+			numberArray.push('Beep!');
+		} else {
+			numberArray.push(element);
+		}
+	}
+	return numberArray;
+}
 
-    //UI LOGIC
-    $(document).ready(function () {
-      $('form').submit(function (event) {
-        event.preventDefault();
-        let numberSubmission = parseInt($('#inputField').val(), 10);
-        let numberArray = checkForMatch(numberSubmission);
-        $('#stringOutput').text(numberArray);
-      });
-    });
+//UI LOGIC
+$(document).ready(function () {
+	$('form').submit(function (event) {
+		event.preventDefault();
+		let numberSubmission = parseInt($('#inputField').val(), 10);
+		let numberArray = checkForMatch(numberSubmission);
+		$('#stringOutput').text(numberArray);
+	});
+});
