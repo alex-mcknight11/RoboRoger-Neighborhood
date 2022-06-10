950ef40cbb69cf5ec11150869b8dd667da21a740
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-06-10 11:35:21 -0700

    add picture and adjust spacing in results

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,11 @@ function checkForMatch(number) {
 	for (let i = 0; number >= i; i++) {
 		const element = i.toString();
 		if (element.includes('3')) {
-			numberArray.push("Won't you be my neighbor?");
+			numberArray.push(" Won't you be my neighbor?");
 		} else if (element.includes('2')) {
-			numberArray.push('Boop!');
+			numberArray.push(' Boop!');
 		} else if (element.includes('1')) {
-			numberArray.push('Beep!');
+			numberArray.push(' Beep!');
 		} else {
 			numberArray.push(element);
 		}
